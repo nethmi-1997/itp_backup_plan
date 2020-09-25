b3fcaf2da1115e1c683cef17bd75ffd4de96e397
Harshi-Dole <63668270+Harshi-Dole@users.noreply.github.com>
2020-09-25 18:01:19 +0530

    Harshi

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+declare interface TableData {
+  headerRow: string[];
+  dataRows: string[][];
+}
+declare interface RouteInfo {
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+}
 
 @Component({
   selector: 'app-documents',
@@ -6,10 +16,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./documents.component.css']
 })
 export class DocumentsComponent implements OnInit {
-
+  public tableData1: TableData;
   constructor() { }
 
+
   ngOnInit(): void {
+    this.tableData1 = {
+      headerRow: [ 'Document Name', 'code', 'status','Type', 'Create Date', 'Department'],
+      dataRows: [
+          ['Product/service requirement documents', '1', 'Completed', 'PDF', '20/08/20', 'HR'],
+          ['Quality Review', '2', 'Completed', 'PDF', '25/08/20', 'MD'],
+          ['Safety Documents', '3', 'Completed', 'PDF', '27/08/20', 'HR'],
+          ['Internal audit program documents', '4', 'Completed', 'PDF', '28/08/20', 'HR'],
+          ['Result of internal audit', '5', 'Completed', 'PDF', '30/08/20', 'HR'],
+          ['Results of management', '6', 'Completed', 'PDF', '25/08/20', 'MnD']
+      ]
+  };
+    
   }
 
 }
