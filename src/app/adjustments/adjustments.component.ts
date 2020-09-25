fc5df5bf423622c60fa36ecaa0d5f2228dfac372
AkilaRanasinghe <akilaranasingha41@gmail.com>
2020-09-25 18:05:06 +0530

    Initial Push

--- src/app/adjustments/adjustments.component.ts
+++ src/app/adjustments/adjustments.component.ts
@@ -17,12 +17,12 @@ export class AdjustmentsComponent implements OnInit {
     this.tableData1 = {
       headerRow: [ 'ID', 'Employee Number', 'Description', 'Date', 'Amount', 'Type'],
       dataRows: [
-        ['1', '1', 'Loan Cut', '2020/09/20 13:05:02', '5000', 'Deduction'],
-        ['2', '2', 'Overtime', '2020/09/22 13:10:24', '10000', 'Addition'],
-        ['3', '2', 'Salary Advance', '2020/09/22 13:12:54', '8000', 'Addition'],
-        ['4', '1', 'Other', '2020/09/25 13:15:04', '5000', 'Deduction'],
-        ['5', '3', 'Overtime', '2020/09/20 13:17:24', '10000', 'Addition'],
-        ['6', '4', 'Holiday Cut', '2020/09/24 13:19:48', '6000', 'Deduction'],
+        ['1', '1', 'Loan Cut', '2020/09/20', '5000', 'Deduction'],
+        ['2', '2', 'Overtime', '2020/09/22', '10000', 'Addition'],
+        ['3', '2', 'Salary Advance', '2020/09/22', '8000', 'Addition'],
+        ['4', '1', 'Other', '2020/09/25', '5000', 'Deduction'],
+        ['5', '3', 'Overtime', '2020/09/20', '10000', 'Addition'],
+        ['6', '4', 'Holiday Cut', '2020/09/24', '6000', 'Deduction'],
       ]
     };
 
--- src/app/pettycash/pettycash.component.ts
+++ src/app/pettycash/pettycash.component.ts
@@ -17,10 +17,10 @@ export class PettycashComponent implements OnInit {
     this.tableData1 = {
       headerRow: [ 'ID', 'Employee Number', 'Reason', 'Date', 'Amount', 'Proof Document'],
       dataRows: [
-        ['1', '1', 'Transport', '2020/09/21 13:10:04', '4000', 'Document 1'],
-        ['2', '1', 'Lunch', '2020/09/22 13:14:26', '3000', 'Document 2'],
-        ['3', '2', 'Sick', '2020/09/22 13:15:25', '7000', 'Document 2'],
-        ['4', '3', 'Pay Expenses', '2020/09/25 13:18:25', '12000', 'Document 3'],
+        ['1', '1', 'Transport', '2020/09/21', '4000', 'Document 1'],
+        ['2', '1', 'Lunch', '2020/09/22', '3000', 'Document 2'],
+        ['3', '2', 'Sick', '2020/09/22', '7000', 'Document 2'],
+        ['4', '1', 'Pay Expenses', '2020/09/25', '10000', 'Document 1'],
       ]
     };
 
